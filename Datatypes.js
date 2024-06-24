49e74a8cd9e610326f561f46e567bdd9c51f2045
Bhavin <Bhavinondhiya0@gmail.com>
2024-06-25 01:36:41 +0530

    Datatypes and ES6

---
+++ Datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code as Newer version ES6
+
+//alert(3+3); //we are using nodejs, so alert will not work
+
+console.log(3+3);
+
+console.log("Bhavin");
+
+let name = "Bhavin";
+let age = 25;
+let isApproved = true;
+let firstName = undefined;
+let selectedColor = null;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isApproved);
+console.log(typeof firstName);
+console.log(typeof selectedColor);
+console.log(typeof null);
